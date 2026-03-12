68c6e813291212c57fc61f8591c78560c5c1e2e6
swatantraj34 <swatantraj34@gmail.com>
2026-03-12 15:41:18 +0530

    arcjet ratelimiting

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -2,10 +2,34 @@ import express from "express";
 import { signup,login,logout } from "../controller/contentapi.js";
 import { protectroute } from "../middleware/auth.middleware.js";
 import { updateProfile } from "../controller/contentapi.js";
+import { arcjetMiddleware } from "../middleware/arcjet.middleware.js";
 const router = express.Router();
+router.get("/test",arcjetMiddleware,(req, res) => { 
+
+res.status(200).json({message: "welcome to auth api"});
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
  
 router.post("/signup",signup);
-router.post("/login",login );
+router.post("/login",arcjetMiddleware,login );
     
 router.put("/update-profile",protectroute, updateProfile);
 
